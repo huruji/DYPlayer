b99c1e5f52a6eb33f788d442d7e5b91700f07d1c
huruji <huruji3@foxmail.com>
2019-04-07 19:38:07 +0800

    feat: :sparkles: add play and pause event

--- src/Events.ts
+++ src/Events.ts
@@ -4,24 +4,44 @@ import Player from './Player'
 export default class Events {
   player: Player
   video: Player['video']
+  videoContainer: Player['videoContainer']
 
   constructor(player: Player) {
     this.player = player
     this.video = this.player.video
+    this.videoContainer = this.player.videoContainer
     this.init()
   }
 
   init() {
-    this.player.videoPlayBtn.addEventListener('click', () => {
-      const playPromise = Promise.resolve(this.video.play())
-      playPromise.catch(() => {
-        this.video.pause()
-      })
+    this.player.videoPlayBtn.addEventListener('click', (e) => {
+      console.log('123')
+      this.playVideo()
+      e.stopPropagation()
     })
 
-    this.video.addEventListener('play', () => {
+    this.video.addEventListener('play', (e) => {
       this.player.videoPlayBtn.style.display = 'none'
-    })
+    }, false)
 
+    this.video.addEventListener('pause', (e) => {
+      e.stopPropagation()
+    }, false)
+
+    this.videoContainer.addEventListener('click', () => {
+      if (this.video.paused) {
+        this.playVideo()
+      } else {
+        this.video.pause()
+        this.player.videoPlayBtn.style.display = 'block'
+      }
+    })
+  }
+
+  playVideo() {
+    const playPromise = Promise.resolve(this.video.play())
+    playPromise.catch(() => {
+      this.video.pause()
+    })
   }
 }
